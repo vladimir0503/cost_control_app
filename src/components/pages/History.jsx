7f8cd46932eba1605a47fee0ae48087a8686e10c
Vladimir <vovan0503@mail.ru>
2020-11-22 23:21:22 +0300

    Add History.jsx

--- src/components/pages/History.jsx
+++ src/components/pages/History.jsx
@@ -6,39 +6,39 @@ import { clearHistory } from "../../redux/actions";
 import Form from "../Form";
 
 function History() {
-  const historyArr = useSelector((state) => state.history);
+    const historyArr = useSelector((state) => state.history);
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  console.log(historyArr);
+    console.log(historyArr);
 
-  const handleClearHistory = () => {
-    dispatch(clearHistory());
-  };
+    const handleClearHistory = () => {
+        dispatch(clearHistory());
+    };
 
-  return (
-    <div className="operationBlock">
-      <Form name="История операций">
-        <div className="formItems">
-          <ul>
-            {historyArr.map((opr) => (
-              <li>
-                <p>{`Дата операции: ${opr.date}`}</p>
-                <p>{`Сумма ${opr.sum}`}</p>
-                <p>{`Комментарий: ${opr.comment}`}</p>
-              </li>
-            ))}
-          </ul>
-          <button onClick={handleClearHistory} className="formBtn">
-            Отчистить историю
+    return (
+        <div className="operationBlock">
+            <Form name="История операций">
+                <div className="formItems">
+                    <ul>
+                        {historyArr.map((opr) => (
+                            <li>
+                                <p>{`Дата операции: ${opr.date}`}</p>
+                                <p>{`Сумма ${opr.sum}`}</p>
+                                <p>{`Комментарий: ${opr.comment}`}</p>
+                            </li>
+                        ))}
+                    </ul>
+                    <button onClick={handleClearHistory} className="formBtn">
+                        Отчистить историю
           </button>
-          <Link to="/">
-            <button className="formBtn">Вернутся на главную</button>
-          </Link>
+                    <Link to="/">
+                        <button className="formBtn">Вернутся на главную</button>
+                    </Link>
+                </div>
+            </Form>
         </div>
-      </Form>
-    </div>
-  );
+    );
 }
 
 export default History;
