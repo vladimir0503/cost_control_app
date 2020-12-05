ac3aaa709bc23e7b771b096aa9149d205d7c87e5
Vladimir <vovan0503@mail.ru>
2020-12-05 21:41:54 +0300

    Add Procifile

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Home from "./components/pages/Home";
 function App() {
   const authData = localStorage.getItem("authData");
 
-  console.log(authData);
-
   return (
     <div>{!authData ? <StartScreen /> : <Home authData={authData} />}</div>
   );
