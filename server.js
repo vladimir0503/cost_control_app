16b226908d7bb51eae0a18c0b9d0b838b911035a
Vladimir <vovan0503@mail.ru>
2020-12-07 18:06:30 +0300

    Fix server.js

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('./public/db.json');
+// const middleWares = jsonServer.defaults({
+//     static: './build',
+// });
+
+// const PORT = process.env.PORT || 3001;
+
+// server.use(middleWares);
+// server.use(router);
+
+// server.listen(PORT, () => {
+//     console.log('Server is running');
+// });
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./public/db.json');
+const router = jsonServer.router('db.json');
 const middleWares = jsonServer.defaults({
     static: './build',
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = Number(process.env.PORT || 3000);
 
 server.use(middleWares);
 server.use(router);
-
 server.listen(PORT, () => {
-    console.log('Server is running');
+    console.log('JSON Server is running')
 });
