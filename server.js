f62b295d2b96b31a1e6b36d6849e7550ad3002d9
Vladimir <vovan0503@mail.ru>
2020-12-09 15:02:41 +0300

    Fix server.js

--- server.js
+++ server.js
@@ -1,42 +1,15 @@
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router('./public/db.json');
-// const middleWares = jsonServer.defaults({
-//     static: './build',
-// });
-
-// const PORT = process.env.PORT || 3001;
-
-// server.use(middleWares);
-// server.use(router);
-
-// server.listen(PORT, () => {
-//     console.log('Server is running');
-// });
-
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const db = {
-    users: [
-        {
-            userName: "Владимир",
-            password: 123455434,
-            total: 0,
-            history: [],
-            id: 0
-        }
-    ]
-}
-
-const router = jsonServer.router(db)
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./public/db.json');
 const middleWares = jsonServer.defaults({
     static: './build',
 });
 
 const PORT = process.env.PORT || 3001;
 
-server.use(middleWares)
-server.use(router)
+server.use(middleWares);
+server.use(router);
+
 server.listen(PORT, () => {
-    console.log('JSON Server is running')
-})
+    console.log('Server is running');
+});
